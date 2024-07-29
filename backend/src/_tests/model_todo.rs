dcf2db8287941530735d7ff44f823bb3a63d0437
Innocent Onyemaenu <rockcoolsaint@gmail.com>
2024-07-29 19:56:40 +0100

    initial commit

---
+++ backend/src/_tests/model_todo.rs
@@ -0,0 +1,26 @@
+use crate::model::db::init_db;
+
+use super::TodoMac;
+
+#[tokio::test]
+async fn model_todo_list() -> Result<(), Box<dyn std::error::Error>> {
+    // -- FIXTURE
+    let db = init_db().await?;
+
+    // -- ACTION
+    let todos = TodoMac::list(&db).await?;
+
+    // CHECK
+    let result = sqlx::query("SELECT * from todo").fetch_all(&db).await?;
+    assert_eq!(2, todos.len());
+    // todo 101
+    assert_eq!(101, todos[0].id);
+    assert_eq!(123, todos[0].cid);
+    assert_eq!("todo 101", todos[0].title);
+    // todo 100
+    assert_eq!(100, todos[1].id);
+    assert_eq!(123, todos[1].cid);
+    assert_eq!("todo 100", todos[1].title);
+
+    Ok(())
+}
---
+++ backend/src/model/todo.rs
@@ -0,0 +1,49 @@
+use super::db::Db;
+use crate::model;
+
+// region:     Todo Types
+#[derive(sqlx::FromRow, Debug, Clone)]
+pub struct Todo {
+  pub id: i64,
+  pub cid: i64, // creator id
+  pub title: String,
+}
+
+pub struct TodoPatch {
+  pub cid: Option<i64>,
+  pub title: Option<String>,
+}
+
+// endregion:  Todo Types
+
+// region:     TodoMac
+pub struct TodoMac;
+
+impl TodoMac {
+  pub async fn create(db: &Db, data:TodoPatch) -> Result<Todo, model::Error> {
+    let sql = "INSERT INTO todo (cid, title) VALUES ($1, $2) returning id, cid, title";
+    let query = sqlx::query_as::<_, Todo>(&sql)
+      .bind(123 as i64)
+      .bind(data.title.unwrap_or_else(|| "untitled".to_string()));
+
+    let todo = query.fetch_one(db).await?;
+
+    Ok(todo)
+  }
+  pub async fn list(db: &Db) -> Result<Vec<Todo>, model::Error> {
+    let sql = "SELECT is, cid, title FROM todo Order BY id DESC";
+    
+    // build the sqlx-query
+    let query = sqlx::query_as(&sql);
+    // execute the query
+    let todos = query.fetch_all(db).await?;
+  
+    Ok(todos)
+  }
+}
+
+// endregion:  TodoMac
+
+#[cfg(test)]
+#[path = "../_tests/model_todo.rs"]
+mod tests;
